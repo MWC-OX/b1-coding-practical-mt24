4c31c548b197331fb554269077423791e3ae39c8
m-Chan24 <140012163+m-Chan24@users.noreply.github.com>
2024-10-16 14:11:01 +0100

    Added framework for controller

---
+++ uuv_mission/control.py
@@ -0,0 +1,17 @@
+
+
+class controller:
+
+    def __init__(self, k_p: float, k_d: float) -> None:
+        self.k_p = k_p
+        self.k_d = k_d
+        self.previous_err = 0
+
+    def update_action(self, reference: float, posistion: float) -> float:
+        err = self._calc_err(reference, posistion)
+        action = (err * self.k_p) + (self.k_d * (err - self.previous_err))
+        self.previous_err = err
+        return action
+
+    def _calc_err(self, reference: float, posistion: float) -> float:
+        return reference - posistion
