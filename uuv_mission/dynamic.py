33ea54ff4ef289992927508102a9582463590190
m-Chan24 <140012163+m-Chan24@users.noreply.github.com>
2024-10-16 12:26:45 +0100

    added functionality for reading mission from csv

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
+
 from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 from .terrain import generate_reference_and_limits
 
 class Submarine:
@@ -76,8 +79,12 @@ class Mission:
     @classmethod
     def from_csv(cls, file_name: str):
         # You are required to implement this method
-        pass
-
+        csv_data = pd.read_csv(file_name)
+        reference = csv_data["reference"].to_numpy()
+        cave_height = csv_data["cave_height"].to_numpy()
+        cave_depth = csv_data["cave_depth"].to_numpy()
+        return cls(reference, cave_height, cave_depth)
+    
 
 class ClosedLoop:
     def __init__(self, plant: Submarine, controller):
